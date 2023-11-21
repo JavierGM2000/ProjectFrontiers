1343d7e863542c9a3506586f5aaec61538220a96
Ander Hernández <ander.hernandez@opendeusto.es>
2023-11-21 11:42:36 +0100

    Plane Movement
    :octopus:

---
+++ Assets/@myassets/scripts/PlaneMovement/PlaneMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlaneMovement : MonoBehaviour
+{
+
+    public PlaneControlActions planeControls;
+    public InputAction pitchAction;
+    public InputAction rollAction;
+    public InputAction yawAction;
+    public float velocityMultiplier;
+    public InputAction thrustAction;
+
+
+    private void Awake()
+    {
+        planeControls = new PlaneControlActions();
+        pitchAction = planeControls.PlaneMap.Pitch;
+        rollAction = planeControls.PlaneMap.Roll;
+        yawAction = planeControls.PlaneMap.Yaw;
+        thrustAction = planeControls.PlaneMap.Thrust;
+        pitchAction.Enable();
+        rollAction.Enable();
+        yawAction.Enable();
+        thrustAction.Enable();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocityMultiplier = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        /*Debug.Log("Pitch = "+pitchAction.ReadValue<float>());
+        Debug.Log("Yaw = " + yawAction.ReadValue<float>());
+        Debug.Log("Roll = " + rollAction.ReadValue<float>());
+        Debug.Log("Thrust = " + thrustAction.ReadValue<float>());
+        Debug.Log("Velocity = " + velocityMultiplier);*/
+
+        velocityMultiplier += thrustAction.ReadValue<float>()*0.2f * Time.deltaTime;
+        velocityMultiplier = Mathf.Clamp(velocityMultiplier, 0, 1);
+        transform.position += transform.forward * velocityMultiplier*Time.deltaTime*15;
+        transform.Rotate(new Vector3(-1*pitchAction.ReadValue<float>()* Time.deltaTime*90, yawAction.ReadValue<float>() * Time.deltaTime * 50, -1*rollAction.ReadValue<float>() * Time.deltaTime * 90));
+    }
+}
