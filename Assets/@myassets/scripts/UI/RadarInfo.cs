f3891b1ed7310843bf50e1959cd7ed0bb3ec4a36
Javier Gomez <javiergm2009@gmail.com>
2023-12-11 20:25:59 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/@myassets/scripts/UI/RadarInfo.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// You! Solitary! NOW!
+// - AWACS Bandog
+public class RadarInfo : MonoBehaviour
+{
+    [SerializeField]
+    int team;
+    [SerializeField]
+    string IFFCode;
+    [SerializeField]
+    string planeName;
+    [SerializeField]
+    string callsign;
+    [SerializeField]
+    bool revealsTeam;
+    
+
+    public int getTeam()
+    {
+        return team;
+    }
+    public bool getRevealsTeam()
+    {
+        return revealsTeam;
+    }
+
+    public string getCallsign()
+    {
+        return callsign;
+    }
+
+    public string getPlaneName()
+    {
+        return planeName;
+    }
+
+    public string getIFFCode()
+    {
+        return IFFCode;
+    }
+
+    public Transform getTransform()
+    {
+        return transform;
+    }
+}
