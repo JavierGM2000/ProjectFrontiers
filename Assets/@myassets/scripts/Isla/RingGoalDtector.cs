17be95a28b76622014064d5b3ffff1f8cacf4351
ThePikaTamer <thepikatamer@gmail.com>
2024-06-08 19:56:10 +0200

    Añadido sonido a la meta e interfaz con puntuaciones
    Hay creados tags para host y client, habrá que asignar cada uno al jugador correspondiente.

--- Assets/@myassets/scripts/Isla/RingGoalDtector.cs
+++ Assets/@myassets/scripts/Isla/RingGoalDtector.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class RingGoalDtector : MonoBehaviour
 {
     public GoalSpawnControll spawner;
+    public RaceGoalScore scoring;
+
+    public AudioSource SoundSource;
+    public AudioClip GoalReachSource;
 
     // Start is called before the first frame update
     void Start()
     {
         spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<GoalSpawnControll>();
+        scoring = GameObject.FindGameObjectWithTag("GameManager").GetComponent<RaceGoalScore>();
+        SoundSource = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,7 +26,21 @@ public class RingGoalDtector : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        spawner.SpawnAtRandom();
-        Destroy(gameObject);
+        if(other.tag=="Host") //crear tags para host y client
+        {
+            scoring.hostScore++; 
+            SoundSource.PlayOneShot(GoalReachSource);
+            spawner.SpawnAtRandom();
+            Destroy(gameObject);
+        }
+        
+        else if(other.tag == "Client")
+        {
+            scoring.clientScore++;
+            SoundSource.PlayOneShot(GoalReachSource);
+            spawner.SpawnAtRandom();
+            Destroy(gameObject);
+        }
+
     }
 }
