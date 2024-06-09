fa728f5c9f88e040ba0edb9ba16d955d6f9716c5
Javier Gomez <gomez.javier@opendeusto.com>
2024-06-09 11:38:28 +0200

    Creado un menu principal

--- Assets/@myassets/scripts/ChangeLevelOnTrigger.cs
+++ Assets/@myassets/scripts/ChangeLevelOnTrigger.cs
@@ -8,13 +8,19 @@ public class ChangeLevelOnTrigger : MonoBehaviour
 {
     public PlaneControlActions planeControls;
     public InputAction missileAction;
+    public InputAction machinegunAction;
+    public InputAction cameraAction;
 
     // Start is called before the first frame update
     void Start()
     {
         planeControls = new PlaneControlActions();
         missileAction = planeControls.PlaneMap.Missile;
+        machinegunAction = planeControls.PlaneMap.Shoot;
+        cameraAction = planeControls.PlaneMap.ResetCam;
+        machinegunAction.Enable();
         missileAction.Enable();
+        cameraAction.Enable();
     }
 
     // Update is called once per frame
@@ -23,6 +29,14 @@ public class ChangeLevelOnTrigger : MonoBehaviour
         if (missileAction.WasPressedThisFrame())
         {
             SceneManager.LoadScene("EscenaFinal");
+        }
+        if (machinegunAction.WasPressedThisFrame())
+        {
+            SceneManager.LoadScene("EscenaFinalConIA");
+        }
+        if (machinegunAction.WasPressedThisFrame())
+        {
+            SceneManager.LoadScene("lobyMultiplayer");
         }
     }
 }
